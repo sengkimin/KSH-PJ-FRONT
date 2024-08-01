9a52e6e94790d8a5ed82f06b3fd0f1e549a90e22
sengkimin <sengin.pse@gmail.com>
2024-08-01 13:54:25 +0700

    improve

--- src/pages/bookCatalog/BookCatalogPage.jsx
+++ src/pages/bookCatalog/BookCatalogPage.jsx
@@ -20,9 +20,9 @@ const BookTable = () => {
     <div className="container mx-auto">
         <h1 className='text-4xl font-bold mb-6'>Book Catalog</h1>
       <button className="bg-blue-500 text-white px-8 py-3 mb-4 rounded-lg hover:bg-stone-400 cursor-pointer">Create</button>
-      <table className="min-w-full bg-white border-2 cursor-pointer ">
+      <table className="min-w-full bg-white border-2 cursor-pointer table-fixed ">
         <thead  className="text-left  bg-stone-100  "  > 
-          <tr className='border-2 border-zinc-200'>
+          <tr className='border-2 border-stone-300'>
             <th className="px-4 py-4 ">Action</th>
             <th className="px-4 py-2">ISBN</th>
             <th className="px-4 py-2 ">Title</th>
@@ -34,16 +34,16 @@ const BookTable = () => {
         </thead>
         <tbody className='text-left'>
           {books.map((book) => (
-            <tr className='border-2'>
-              <td className="px-4 py-4 ">
+            <tr className='border-2 border-stone-300' >
+              <td className="px-4 py-6 ">
                 <button className="bg-blue-400 text-white px-8 py-2 rounded-lg hover:bg-stone-400 ">View</button>
               </td>
-              <td className="px-4 py-2 ">{book.isbn}</td>
-              <td className="px-4 py-2 ">{book.title}</td>
-              <td className="px-4 py-2 ">{book.authors}</td>
-              <td className="px-4 py-2 ">{book.publisher}</td>
-              <td className="px-4 py-2 ">{book.genre}</td>
-              <td className="px-4 py-2 ">{book.shelf_location}</td>
+              <td className="px-4  ">{book.isbn}</td>
+              <td className="px-4 ">{book.title}</td>
+              <td className="px-4 ">{book.authors}</td>
+              <td className="px-4 ">{book.publisher}</td>
+              <td className="px-4  ">{book.genre}</td>
+              <td className="px-4  ">{book.shelf_location}</td>
             </tr>
           ))}
         </tbody>
