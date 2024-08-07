0066157aba70d1af6eda0531da5c9600a2655400
saolaiheang <laiheang.sao@institute.pse.ngo>
2024-08-07 15:28:16 +0700

    view delete useraccount

--- src/pages/userAcc/ViewUserAccountunfo.jsx
+++ src/pages/userAcc/ViewUserAccountunfo.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import BoxInfo from '../../components/BoxInfo';
+import BoxInfo from '../../components/BoxInformation';
 import Btn from '../../components/Btn';
 import { Link } from 'react-router-dom';
 const ViewUserAccountinfo = () => {
