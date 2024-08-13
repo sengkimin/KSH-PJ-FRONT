942950099bc2d330c24e3d2b12970841c961f3b0
saolaiheang <laiheang.sao@institute.pse.ngo>
2024-08-13 08:41:03 +0700

    pagenation user accounts

--- src/api/member.js
+++ src/api/member.js
@@ -24,3 +24,5 @@ export const fetchMember = async () => {
         return [];
     }
 };
+
+
--- src/pages/userAcc/UserAcounts.jsx
+++ src/pages/userAcc/UserAcounts.jsx
@@ -5,6 +5,8 @@ import Btn from '../../components/Buttons';
 import { Link } from 'react-router-dom';
 function UserAccountListPage() {
     const [users, setUsers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(10);
     const token = localStorage.getItem("token");
     useEffect(() => {
         const fetchUsers = async () => {
@@ -26,6 +28,20 @@ function UserAccountListPage() {
         };
         fetchUsers();
     }, []);
+    const lastUser = currentPage * itemsPerPage;
+    const firstUser = lastUser - itemsPerPage;
+    const currentUser = users.slice(firstUser, lastUser);
+    const totalPage = parseInt(users.length / itemsPerPage)+1;
+    function nextPage() {
+        if (currentPage < totalPage) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+    function handlePrev() {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
 
     return (
         <div>
@@ -48,10 +64,10 @@ function UserAccountListPage() {
                         </tr>
                     </thead>
                     <tbody >
-                        {users.map((user) => (
+                        {currentUser.map((user) => (
 
                             <tr key={user.id} >
-                                <td className="py-4 px-4 border-b"> <Link to={`/user-account/${user.id}`} className='px-8 py-2 bg-blue-400 rounded'>view</Link></td>
+                                <td className="py-4 px-4 border-b"> <Link to={`/user-account/${user.id}`} className='px-8 py-2 bg-blue-400 text-white rounded'>view</Link></td>
                                 <td className="py-4 px-4 border-b">{user.id}</td>
                                 <td className="py-4 px-4 border-b">{user.username}</td>
                                 <td className="py-4 px-4 border-b">{user.email}</td>
@@ -65,9 +81,17 @@ function UserAccountListPage() {
                     </tbody>
 
                 </table>
+                <div className='flex justify-end mt-4'>
+                    <button onClick={handlePrev} disabled={currentPage === 1} className={`px-4 py-2 rounded   ${currentPage === 1 ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 text-white'}`} > Prev</button>
+                    <span className='p-4'> {currentPage} </span>
+                    <button onClick={nextPage } disabled={currentPage === totalPage} className={`px-4 py-2 rounded ${currentPage === totalPage ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 text-white'}`}>Next </button>
+                </div>
             </div>
         </div>
     );
 }
 
 export default UserAccountListPage;
+
+
+
