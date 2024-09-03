89fcc834c742c22a735506c2f975921b1dad548f
sengkimin <sengin.pse@gmail.com>
2024-09-03 14:56:37 +0700

    improve sidebar

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -30,44 +30,68 @@ function AppLayout() {
             isSidebarOpen ? "translate-x-0" : "-translate-x-full"
           } md:static md:translate-x-0 md:w-80`}
         >
-          <div className="p-5 text-center text-2xl text-green-700 font-semibold border-b border-gray-700">
+          <div className="p-5 text-center text-3xl md:text-5xl text-[#207137] font-semibold border-b border-gray-700">
             KSH
           </div>
           <nav className="mt-4">
-       
             <Link
               to="/"
-              className="block text-xl font-bold text-black py-4 px-4 hover:bg-green-800 hover:text-white"
+              className="block text-xl md:text-2xl font-bold text-[#474747] py-4 md:py-6 px-4 hover:bg-[#207137] hover:text-white flex items-center"
               onClick={closeSidebar}
             >
-              Residents List
+              <img
+                src="/resident.png"
+                alt="Residents"
+                className="h-16 w-15 md:w-17 mr-6"
+              />
+              Residents
             </Link>
             <Link
               to="/program"
-              className="block text-xl font-bold text-black py-4 px-4 hover:bg-green-800 hover:text-white"
+              className="block  text-xl md:text-2xl font-bold text-[#474747] py-4 md:py-6 px-4 hover:bg-[#207137] hover:text-white  flex items-center"
               onClick={closeSidebar}
             >
+                      <img
+                src="/program.png"
+                alt="Residents"
+                className=" w-15 h-16 md:w-18 mr-6"
+              />
               Programs
             </Link>
             <Link
               to="/assesment"
-              className="block text-xl font-bold text-black py-4 px-4 hover:bg-green-800 hover:text-white"
+              className="block  text-xl md:text-2xl font-bold text-[#474747] py-4 md:py-6 px-4 hover:bg-[#207137] hover:text-white  flex items-center"
               onClick={closeSidebar}
             >
+                      <img
+                src="/assesment.png"
+                alt="Residents"
+                className="h-16 w-20 mr-10"
+              />
               Assessment
             </Link>
             <Link
               to="/reporting"
-              className="block text-xl font-bold text-black py-4 px-4 hover:bg-green-800 hover:text-white"
+              className="block  text-xl md:text-2xl font-bold text-[#474747] py-1 md:py-6 px-4 hover:bg-[#207137] hover:text-white  flex items-center"
               onClick={closeSidebar}
             >
+                          <img
+                src="/report.png"
+                alt="Residents"
+                className="h-17 w-20 mr-10"
+              />
               Reporting
             </Link>
             <Link
               to="/medical"
-              className="block text-xl font-bold text-black py-4 px-4 hover:bg-green-800 hover:text-white"
+              className="block  text-xl md:text-2xl font-bold text-[#474747] py-2 md:py-6 px-4 hover:bg-[#207137] hover:text-white  flex items-center"
               onClick={closeSidebar}
             >
+                      <img
+                src="/medicals.png "
+                alt="Residents"
+                className="h-17 w-20 mr-10"
+              />
               Medical
             </Link>
           </nav>
@@ -79,7 +103,7 @@ function AppLayout() {
               <img
                 src="/khslogo.jpg"
                 alt="Logo"
-                className="h-10 w-10 rounded-full"
+                className=" h-11 w-11 md:h-14 md:w-14 rounded-full"
               />
             </div>
             <div className="flex items-center space-x-4">
@@ -91,9 +115,6 @@ function AppLayout() {
               >
                 <FaBars className="w-6 h-6" />
               </button>
-
-           
-         
 
               <button className="bg-gray-400 text-white p-2 md:p-3 rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
                 <FaUser className="w-4 h-4" />
@@ -117,6 +138,3 @@ function AppLayout() {
 }
 
 export default AppLayout;
-
-
-
