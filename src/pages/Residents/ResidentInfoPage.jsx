b77eb54be283ee51624aac3cd55c8eda5b6f9c06
sengkimin <sengin.pse@gmail.com>
2024-09-05 14:52:14 +0700

    residentinfo

--- src/pages/Residents/ResidentInfoPage.jsx
+++ src/pages/Residents/ResidentInfoPage.jsx
@@ -22,16 +22,14 @@ const ResidentInfo = () => {
 
   return (
     <>
-      <div className="flex space-x-4 mb-8 mt-6 ml-4">
+      <div className="flex space-x-4 mb-8 mt-6 ml-6">
         <Link to='/'>
-          <button className="bg-gray-300 px-6 py-1.5 md:px-12 md:py-2 rounded">
+          <button className="bg-gray-300 px-6 py-1.5 md:px-16 md:py-2 rounded">
             Back
           </button>
         </Link>
         
-        <button className="bg-green-700 text-white px-6 py-1.5 md:px-10 md:py-2 rounded">
-          Activities
-        </button>
+  
       </div>
       
       <div className="flex justify-center mb-6">
