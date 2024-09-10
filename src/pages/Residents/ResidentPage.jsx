4ad79cf2da8bda8e2c0306e00b47d741029d0c5d
sengkimin <sengin.pse@gmail.com>
2024-09-10 09:58:50 +0700

    callimage

--- src/pages/Residents/ResidentPage.jsx
+++ src/pages/Residents/ResidentPage.jsx
@@ -7,7 +7,7 @@ import { useLocation } from 'react-router-dom';
 const ResidentList = () => {
   const [residents, setResidents] = useState([]);
   const location = useLocation();
-  const { type } = location.state || { type: "1" }; // Default to "1" if type is not provided
+  const { type } = location.state || { type: "1" }; 
 
   useEffect(() => {
     const fetchResidents = async () => {
@@ -22,10 +22,12 @@ const ResidentList = () => {
       const url = `http://localhost:1337/api/curriculum-program-levels?filters[program_level][program_level_name][$eq]=Level%20${type}&filters[curriculum][end_date][$lte]=${year}-${month}-${day}&populate[residents][populate]=profile_img_url&populate=*`;
 
       try {
+        console.log('Fetching data from:', url);
         const response = await axios.get(url);
+        console.log('API Response:', response);
         const data = response.data?.data || [];
+        console.log('Formatted Data:', data);
         setResidents(data);
-        console.log(data);
       } catch (error) {
         console.error('Error fetching the residents data:', error);
       }
@@ -51,9 +53,12 @@ const ResidentList = () => {
           {residents.length > 0 ? (
             residents.map((resident) =>
               resident.attributes.residents.data.map((EachResident) => {
-                const profileImgUrl = EachResident.profile_img_url?.data?.attributes?.url
-                  ? `http://localhost:1337${EachResident.profile_img_url.data.attributes.url}`
+                const profileImgUrl = EachResident.attributes.profile_img_url?.data?.attributes?.url
+                  ? `http://localhost:1337${EachResident.attributes.profile_img_url.data.attributes.url}`
                   : null;
+
+               
+                console.log('Profile Image URL:', profileImgUrl);
 
                 return (
                   <BoxResident
