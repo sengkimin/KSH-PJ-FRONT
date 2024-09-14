0fe6a4be8f1744d7b2053d7a95fea34a9cb303c3
sengkimin <sengin.pse@gmail.com>
2024-09-14 10:09:21 +0700

    update main branch solve conflict

--- src/pages/Residents/ResidentPage.jsx
+++ src/pages/Residents/ResidentPage.jsx
@@ -23,12 +23,11 @@ const ResidentList = () => {
       const url = `http://localhost:1337/api/curriculum-program-levels?filters[program_level][program_level_name][$eq]=Level%20${type}&filters[curriculum][end_date][$lte]=${year}-${month}-${day}&populate[residents][populate]=profile_img_url&populate=*`;
 
       try {
-        const response = await axios.get('http://localhost:1337/api/beneficiaries',{
+        const response = await axios.get(url,{
           headers: {
             'Authorization': `Bearer ${token}`,
             },
-        })
-        const response = await axios.get(url);
+        });
         console.log('API Response:', response);
         const data = response.data?.data || [];
         console.log('Formatted Data:', data);
